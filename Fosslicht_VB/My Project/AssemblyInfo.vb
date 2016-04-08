eda1a523c78a77492cb68728d5cc57d2e4451f12
Niels de Vries <304838@ubboemmius.net>
2016-04-08 12:26:53 +0200

    Initial work

---
+++ Fosslicht_VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Fosslicht_VB")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Ubbo Emmius")> 
+<Assembly: AssemblyProduct("Fosslicht_VB")> 
+<Assembly: AssemblyCopyright("Copyright © Ubbo Emmius 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aee18d3c-5ae9-4d76-a9c6-fde344147814")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
